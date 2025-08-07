4f042ed7be702d5c05d4c7b10569daa1f170c265
Sahal Ubaidillah Gunardo <sahalubaidillah3@gmail.com>
2025-08-07 09:04:04 +0700

    skill

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector("")
+
+
+
+// Scroll infinity
+const logo = document.querySelector(".scroll-item").cloneNode(true);
+
+document.querySelector(".skill-item").appendChild(logo)
