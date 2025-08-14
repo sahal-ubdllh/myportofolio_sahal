272440e409f7e1ce0520b6cfa71992454cae1342
Sahal Ubaidillah Gunardo <sahalubaidillah3@gmail.com>
2025-08-14 21:46:01 +0700

    perubahan navbar

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-let menuList = document.getElementById("menuList");
-menuList.style.maxHeight = "0px";
+const sidebar = document.querySelector(".sidebar")
 
-function toggleMenu() {
-    if(menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "300px";
-    }else {
-        menuList.style.maxHeight = "0px";
-    }
+function showSidebar() {
+    sidebar.style.display = "flex"
+
+}
+function hideSidebar() {
+    sidebar.style.display = "none"
 }
 
+window.addEventListener("scroll", function() {
+    if (sidebar.style.display === "flex") {
+        hideSidebar(); // Menyembunyikan sidebar ketika menggulir
+    }
+});
+
+window.onload = function() {
+    const elements = document.querySelectorAll('.fade-in'); // Menargetkan elemen dengan kelas fade-in
+    elements.forEach(element => {
+        element.classList.add('fade-in'); // Menambahkan kelas untuk memicu animasi
+    });
+};
+
+window.onload = function() {
+    const elements = document.querySelectorAll('.zoom-in'); // Menargetkan elemen dengan kelas zoom-in
+    elements.forEach(element => {
+        element.classList.add('zoom-in'); // Menambahkan kelas untuk memicu animasi
+    });
+};
+    
+
+   var options = {
+       strings: ["Sahal Ubaidillah Gunardo"],
+       typeSpeed: 50,
+       backSpeed: 50,
+       loop: true // Optional: loops the typing effect 
+   };
+   var typed = new Typed("#typed", options);
 
 
 // Scroll infinity
